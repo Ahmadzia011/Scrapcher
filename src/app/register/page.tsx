ba1182589f2cef857e2fa3dd187b6d46bf94185e
Ahmad zia <ahmadzia0304@gmail.com>
2026-08-25 09:21:54 +0500

    Updated buttons

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -23,8 +23,8 @@ export default function RegisterPage() {
   }
 
   return (
-    <main className="relative flex min-h-screen items-center justify-center overflow-hidden bg-(--primary-color) px-5 py-10">
-      <div className="pointer-events-none absolute -top-40 left-1/2 h-80 w-80 -translate-x-1/2 rounded-full bg-[#78AEFF]/20 blur-3xl" />
+    <main className="relative flex min-h-screen items-center justify-center overflow-hidden bg-linear-to-b from-(--primary-color) from-0% via-[#DAE3ED] via-74% to-[#B7CAE0] to-100%   px-5 py-10">
+      <div className="pointer-events-none absolute -top-40 left-1/2 h-80 w-80 -translate-x-1/2 rounded-full " />
 
       <div className="relative w-full max-w-sm">
         <div className="mb-10 text-center">
